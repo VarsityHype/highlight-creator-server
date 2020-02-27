d7fbbd0bcbfbb704458ac9d990c379725b1e6d9c
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-02-26 22:06:59 -0600

    stashing changes

--- routes/video.js
+++ routes/video.js
@@ -1,7 +1,10 @@
+//598430
+
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
 const models = require('../models')
+const IncomingForm = require('formidable').IncomingForm
 
 router.use(express.urlencoded({extended: false}))
 router.use(express.json())
@@ -11,12 +14,10 @@ router.use(cors())
 
 router.post('/upload', (req, res) => {
 
-    console.log(req.body.title)
-
     let video = models.Videos.build({
         title: req.body.title,
         description: req.body.description,
-        azure_url: req.body.azure_url,
+       // azure_url: req.body.azure_url,
         uploader_id: req.body.uploader_id
     })
     let savedVideo = video.save()
