5357d7e87adb3dac87854d24e7302749a9ba1ddc
Baylie <bayliewarrick2019@gmail.com>
2020-03-02 15:27:28 -0600

    Added ability to view personally owned videos, clips, AND playlists to server sided functions.

---
+++ routes/clips.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const cors = require('cors')
+const models = require('../models')
+
+
+// http://localhost:3001/clips/<clip ID>
+router.get('/:id', (req, res) => {
+    id = req.params.id
+    models.clips.findOne({where: { id: id }})
+    .then(clip => 
+        res.json(clip)
+        )
+})
+
+
+
+module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,43 @@ router.post('/finish-profile', (req, res) => {
     let user = models.Users.findOne({where: { auth0id: auth0id }})
 })
 
+// http://localhost:3001/user/<USER_ID>/my_videos
+// TODO change user ID from a param to the session variable.
+router.get('/:id/my_videos', (req, res) => {
+    let id = req.params.id
+    models.Videos.findAll({ where: {uploader_id: id} })
+    .then(
+        videos => {
+            res.json(videos)
+        }
+    )
+})
+
+// http://localhost:3001/user/<USER_ID>/my_clips
+router.get('/:id/my_clips', (req, res) => {
+    let id = req.params.id
+    models.Clips.findAll({ where: {creator_id: id} })
+    .then(
+        videos => {
+            res.json(videos)
+        }
+    )
+})
+
+// http://localhost:3001/user/<USER_ID>/my_playlists
+router.get('/:id/my_playlists', (req, res) => {
+    let id = req.params.id
+    models.Clips.findAll({ where: {owner_id: id} })
+    .then(
+        videos => {
+            res.json(videos)
+        }
+    )
+})
+
+
+
+
 
 module.exports = router
 
